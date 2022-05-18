5b5b7478b912904b12960f4595788c5bc156d1e4
JM Carizon <jhon_carizon@cody.inc>
2022-05-18 17:59:57 +0800

    heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,5 @@
 console.log('Client side!')
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=Canduman').then((response) => {
-//     response.json().then((data) => {
-//         // console.log(data)
-//         if(data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.forecast)
-//             console.log(data.location)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
@@ -31,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     msg1.textContent = 'Loading...'
     msg2.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 msg1.textContent = data.error
