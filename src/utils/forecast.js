6094fb6c8e812c2ea2f683ee9f2edfee15c8279a
JM Carizon <jhon_carizon@cody.inc>
2022-05-19 10:27:04 +0800

    api update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,18 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=7995dcb22591c8d430a01a239d58c5d4&query=' + lat + ',' + long
+    // const url = 'http://api.weatherstack.com/current?access_key=7995dcb22591c8d430a01a239d58c5d4&query=' + lat + ',' + long
+    const url = 'http://api.weatherapi.com/v1/current.json?key=62562f74a3e94c2d9a320510221905&q=' + lat + ',' + long + '8&aqi=no'
 
     request({ url, json: true }, (error, { body }) => {
-        const {weather_descriptions, temperature, feelslike} = body.current
+        const {condition, temp_c, feelslike_c} = body.current
         if(error) {
             callback('Could not get to the service!', undefined) 
         } else if(body.error) {
             callback('Location not found!', undefined)
         } else {
             callback(undefined, 
-                'Today is ' + weather_descriptions[0] + '. The temperature is ' + temperature + ' but it feels like ' + feelslike + '.'
+                'Today is ' + condition.text + '. The temperature is ' + temp_c + ' but it feels like ' + feelslike_c + '.'
             )
         }
     })
